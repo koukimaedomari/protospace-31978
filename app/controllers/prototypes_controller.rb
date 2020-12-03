29664b74508b02a34a3de52835d2030d987818cb
koukimaedomari <0uh3263022m0t8c@ezweb.ne.jp>
2020-12-03 09:46:55 +0900

    commit

--- app/controllers/prototypes_controller.rb
+++ app/controllers/prototypes_controller.rb
@@ -1,8 +1,7 @@
 class PrototypesController < ApplicationController
-  before_action :set_prototype, only: :edit
+  before_action :set_prototype, only: [:edit]
   before_action :authenticate_user!, except: [:index, :show]
   before_action :move_to_index, only: [:edit, :update, :destroy]
-
 
   def index
     @prototypes = Prototype.includes(:user)
@@ -13,7 +12,7 @@ class PrototypesController < ApplicationController
   end
 
   def create
-    @prototype = Prototype.create(prototype_params)
+    @prototype = Prototype.new(prototype_params)
     if @prototype.save
       redirect_to root_path
     else
